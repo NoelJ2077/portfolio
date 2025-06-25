16bc6b305ffe837eb63baa0c45a097a30bcfc215
Noël Jörg <noelgamemc@gmail.com>
2025-06-25 18:34:00 +0200

    Update text on Project 3

--- assets/js/lastUpdated.js
+++ assets/js/lastUpdated.js
@@ -1,6 +1,6 @@
 // lastUpdated.js
 // update date when changes are pushed!
-const lastUpdated = "24 June 2025";
+const lastUpdated = "25 June 2025";
 // insert into p tag
 document.addEventListener("DOMContentLoaded", function() {
   var el = document.querySelector(".last-updated"); // p tag
