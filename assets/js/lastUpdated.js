be65b9657282e798a7253b3aa0ccec1554eca1db
Noël Jörg <noelgamemc@gmail.com>
2025-12-08 13:39:04 +0100

    Fr25 update, added 1 project.

--- assets/js/lastUpdated.js
+++ assets/js/lastUpdated.js
@@ -1,10 +1,16 @@
 // lastUpdated.js
 // update date when changes are pushed!
-const lastUpdated = "25 June 2025";
+const lastUpdated = "12 December 2025";
+const copyrightdate = "&copy; Copyright 2024 - 2025 <strong>Noël Jörg</strong>"
 // insert into p tag
 document.addEventListener("DOMContentLoaded", function() {
   var el = document.querySelector(".last-updated"); // p tag
+  var elp = document.querySelector(".copyright")
   if (el) {
     el.textContent = "Last updated: " + lastUpdated;
   }
+  if (elp) {
+    elp.innerHTML = copyrightdate
+  }
 });
+
